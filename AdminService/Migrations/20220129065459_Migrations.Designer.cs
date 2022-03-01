a387ae0cb0680b123ec576cd68499d8ccff41a5f
Roval-Benny <rovalbenny@gmail.com>
2022-03-01 15:07:51 +0530

    MySalon Backend

--- AdminService/Migrations/20220129065459_Migrations.Designer.cs
+++ AdminService/Migrations/20220129065459_Migrations.Designer.cs
@@ -3,14 +3,16 @@ using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdminService.Migrations
 {
     [DbContext(typeof(MySalonDbContext))]
-    partial class MySalonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220129065459_Migrations")]
+    partial class Migrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AdminService/Migrations/20220129065459_Migrations.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminService.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Offer",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceName",
+                table: "Admins");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Offer",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Price",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceName",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
